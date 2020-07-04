ac9db560f6e07869d821375a8a60eccda45d29f0
ballcyber53 <38312308+ballcyber53@users.noreply.github.com>
2020-07-04 22:51:08 +0700

    update

--- database/migrations/2020_06_30_071249_create_users_table.php
+++ database/migrations/2020_06_30_071249_create_users_table.php
@@ -23,9 +23,9 @@ class CreateUsersTable extends Migration
             $table->string('phone')->nullable();
             $table->string('school')->nullable();
             $table->string('education_background');
-            $table->unsignedBigInteger('faculty_id');
+            $table->unsignedBigInteger('faculty_id')->nullable();
             $table->foreign('faculty_id')->references('id')->on('faculties');
-            $table->unsignedBigInteger('major_id');
+            $table->unsignedBigInteger('major_id')->nullable();
             $table->foreign('major_id')->references('id')->on('majors');
             $table->string('password');
             $table->rememberToken();
