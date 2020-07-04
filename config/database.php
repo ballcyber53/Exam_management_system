6a55fab86308daf81c5c487466bd52943bc3eb32
ballcyber53 <38312308+ballcyber53@users.noreply.github.com>
2020-07-04 23:48:35 +0700

    config

--- config/database.php
+++ config/database.php
@@ -45,11 +45,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-east-02.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel58'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'B5301518'),
+            'database' => env('DB_DATABASE', 'heroku_c64c7f496c5abab'),
+            'username' => env('DB_USERNAME', 'ba8a2ed42f5d00'),
+            'password' => env('DB_PASSWORD', '8f49b25d'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
